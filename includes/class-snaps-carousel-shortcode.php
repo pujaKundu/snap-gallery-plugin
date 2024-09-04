b2fd164c64cfea422c708164a7d69d169f8e3e2b
pujaKundu <pujakundu797@gmail.com>
2024-09-04 21:54:46 +0600

    style update

--- includes/class-snaps-carousel-shortcode.php
+++ includes/class-snaps-carousel-shortcode.php
@@ -1,5 +1,87 @@
 <?php
 
+// class Snaps_Carousel_Shortcode {
+
+//     public static function register_shortcode() {
+//         add_shortcode('snaps_carousel', array(__CLASS__, 'render_carousel'));
+//     }
+
+//     public static function render_carousel($atts) {
+   
+//     $args = array(
+//         'post_type' => 'snaps_gallery',
+//         'posts_per_page' => -1, 
+//     );
+
+//     $query = new WP_Query($args);
+//     $output = '';
+
+//     if ($query->have_posts()) {
+//         $output .= '<div class="snaps-carousel">';
+        
+//         while ($query->have_posts()) {
+//             $query->the_post();
+//             $post_id = get_the_ID();
+//             $post_title = get_the_title();
+//             $post_link = get_permalink();
+//             $featured_image = get_the_post_thumbnail($post_id, 'medium'); 
+
+//             $output .= '<div class="carousel-item">';
+//             $output .= '<a href="' . esc_url($post_link) . '">';
+//             $output .= $featured_image;
+//             $output .= '<h3>' . esc_html($post_title) . '</h3>'; 
+//             $output .= '</a>';
+//             $output .= '</div>';
+//         }
+
+//         $output .= '</div>';
+
+//         $output .= '<button class="carousel-control prev">Previous
+//         </button>';
+//         $output .= '<button class="carousel-control next">Next</button>';
+
+//         wp_reset_postdata(); 
+//     } else {
+//         $output .= '<p>No gallery posts found.</p>';
+//     }
+
+//     $output .= '<script>
+//         document.addEventListener("DOMContentLoaded", function() {
+//             const items = document.querySelectorAll(".snaps-carousel .carousel-item");
+//             const nextButton = document.querySelector(".carousel-control.next");
+//             const prevButton = document.querySelector(".carousel-control.prev");
+//             let currentItem = 0;
+//             const totalItems = items.length;
+
+//             function showItem(index) {
+//                 items.forEach((item, i) => {
+//                     item.style.display = (i === index) ? "block" : "none";
+//                 });
+//             }
+
+//             function nextItem() {
+//                 currentItem = (currentItem + 1) % totalItems;
+//                 showItem(currentItem);
+//             }
+
+//             function prevItem() {
+//                 currentItem = (currentItem - 1 + totalItems) % totalItems;
+//                 showItem(currentItem);
+//             }
+
+//             showItem(currentItem);
+
+//             nextButton.addEventListener("click", nextItem);
+//             prevButton.addEventListener("click", prevItem);
+
+//             setInterval(nextItem, 3000);
+//         });
+//     </script>';
+
+//     return $output;
+// }
+// }
+
 class Snaps_Carousel_Shortcode {
 
     public static function register_shortcode() {
@@ -17,6 +99,8 @@ class Snaps_Carousel_Shortcode {
     $output = '';
 
     if ($query->have_posts()) {
+        $output .= '<div class="snaps-carousel-container">'; // Added container
+
         $output .= '<div class="snaps-carousel">';
         
         while ($query->have_posts()) {
@@ -36,8 +120,15 @@ class Snaps_Carousel_Shortcode {
 
         $output .= '</div>';
 
-        $output .= '<button class="carousel-control prev">Previous</button>';
-        $output .= '<button class="carousel-control next">Next</button>';
+        $output .= '<button class="carousel-control prev">
+        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd">
+                    <path d="M20 .755l-14.374 11.245 14.374 11.219-.619.781-15.381-12 15.391-12 .609.755z" fill="#f7f7f7"/>
+                    </svg></button>';
+        $output .= '<button class="carousel-control next">
+         <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M4 .755l14.374 11.245-14.374 11.219.619.781 15.381-12-15.391-12-.609.755z" fill="#f7f7f7"/></svg>
+        </button>';
+
+        $output .= '</div>'; 
 
         wp_reset_postdata(); 
     } else {
